5502dd06f3755e7e3b43b8429a40aff08108b53e
Bruno Gil <brunousmc13@gmail.com>
2024-03-07 12:43:45 +0100

    Añadida la clase "Division"

---
+++ src/Division.java
@@ -0,0 +1,11 @@
+public class Division {
+    double num1;
+    double num2;
+    public static double division(double num1,double num2){
+        if(num2==0){
+            return 0;
+        } else {
+            return num1/num2;
+        }
+    }
+}
