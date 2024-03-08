4393ecb53b381c5e6a5853a4d7434bf1e52eabf4
Bruno Gil <brunousmc13@gmail.com>
2024-03-08 10:36:32 +0100

    Creada clase de testMulti mas 3 pruebas

---
+++ tests/testMultiplicacion.java
@@ -0,0 +1,41 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class testMultiplicacion {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void multiplicacion() {
+        double num1 = 10.0;
+        double num2 = 10.0;
+        double expResult = 100.0;
+        double result = Calculadora.Multiplicacion(num1,num2);
+        assertEquals(expResult,result);
+    }
+    @Test
+    void multiplicacion1() {
+        double num1 = 7.0;
+        double num2 = 9.0;
+        double expResult = 63.0;
+        double result = Calculadora.Multiplicacion(num1,num2);
+        assertEquals(expResult,result);
+    }
+    @Test
+    void multiplicacion2() {
+        double num1 = 24.0;
+        double num2 = 3.0;
+        double expResult = 72.0;
+        double result = Calculadora.Multiplicacion(num1,num2);
+        assertEquals(expResult,result);
+    }
+}
